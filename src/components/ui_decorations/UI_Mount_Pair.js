d4a17d5d6ebcf3afff5b3781ffffac7b4a1077bb
mbajor14 <michael.bajor@protonmail.com>
2019-06-25 01:23:43 -0400

    Changed style of ui bracket mounts

--- src/components/ui_decorations/UI_Mount_Pair.js
+++ src/components/ui_decorations/UI_Mount_Pair.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const UI_Mount_Pair = () => (
     <div className="ui_mount">
-        <div className="ui_mount_bracket" />
-        <div className="ui_mount_bracket" />
+        <div className="ui_mount_bracket_outer">
+            <div className="ui_mount_bracket_inner" />
+        </div>
+        <div className="ui_mount_bracket_outer">
+            <div className="ui_mount_bracket_inner" />
+        </div>
     </div>
 );
 
